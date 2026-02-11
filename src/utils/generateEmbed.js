bec87606a317ed9d9b7ca9a83db5b42476cf0beb
Shreyash2001 <shreyashkatare21@gmail.com>
2026-02-12 01:02:37 +0530

    fix: version prefix added

--- src/utils/generateEmbed.js
+++ src/utils/generateEmbed.js
@@ -1,7 +1,7 @@
 export function generateEmbedCode(effectName, config) {
   const username = "Shreyash2001";
   const repo = "dust-text-effect";
-  const version = "1.0.0";
+  const version = "v1.0.0";
 
   const configString = JSON.stringify(config).replace(/"/g, "&quot;");
   const url = `https://cdn.jsdelivr.net/gh/${username}/${repo}@${version}/dist/${effectName}.umd.js`;
