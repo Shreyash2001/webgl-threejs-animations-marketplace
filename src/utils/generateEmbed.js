85759750512c9cd0fde4f525b5bb26cfb659dfe9
Shreyash2001 <shreyashkatare21@gmail.com>
2026-02-12 01:07:02 +0530

    fix: fixed the script url issue

--- src/utils/generateEmbed.js
+++ src/utils/generateEmbed.js
@@ -4,7 +4,7 @@ export function generateEmbedCode(effectName, config) {
   const version = "v1.0.0";
 
   const configString = JSON.stringify(config).replace(/"/g, "&quot;");
-  const url = `https://cdn.jsdelivr.net/gh/${username}/${repo}@${version}/dist/${effectName}.umd.js`;
+  const url = `https://cdn.jsdelivr.net/gh/${username}/${repo}@${version}/dist/${effectName}.umd.cjs`;
 
   return `<script src="${url}" data-config="${configString}" defer></script>`;
 }
